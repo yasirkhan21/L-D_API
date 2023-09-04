dd61d36afb610bac68dfdf08c4466971d52a56ab
Yasir khan <yk1809786@gmail.com>
2023-09-04 16:48:26 +0530

    Basic added

---
+++ LD_API/LD_API.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LD_API.Domain.Entities
+{
+    internal class User
+    {
+    }
+}
